2bd50f0405942d47d7a99e3118918d64e5507274
Morgan Ogola <morganogola@gmail.com>
2024-10-08 11:10:19 +0300

    first commit

---
+++ src/test/java/com/example/eventStore/EventStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
